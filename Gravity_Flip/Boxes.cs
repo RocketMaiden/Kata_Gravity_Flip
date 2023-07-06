b4e878599a4b404bd64aedee3f701801baefa1a7
RocketMaiden <elizabeth.ruchko@gmail.com>
2023-07-06 12:54:11 +0300

    constructor for boxes created

---
+++ Gravity_Flip/Boxes.cs
@@ -0,0 +1,12 @@
+﻿namespace Gravity_Flip
+{
+    public class Boxes
+    {
+        private int[,] boxes_vertical;
+
+        public Boxes(int[] boxes_vertical)
+        {
+            this.boxes_vertical = new int [boxes_vertical.Length - 1 , boxes_vertical.Max()];
+        }
+    }
+}
