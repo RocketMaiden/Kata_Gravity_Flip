9a816a26868d84c03b38056c815a8fcdf0ed87d9
RocketMaiden <elizabeth.ruchko@gmail.com>
2023-07-08 12:16:10 +0300

    subtask with numbers

--- Gravity_Flip/Program.cs
+++ Gravity_Flip/Program.cs
@@ -8,3 +8,28 @@ boxes.ShowRawBoxes();
 
 Console.WriteLine();
 Console.ReadKey();
+
+int number = 1813;
+int length = 0;
+int[] number_array;
+
+while(number%10 > 0)
+{
+    //int letter = number / 10;
+    number = number % 10;
+    length++;
+}
+
+number_array = new int[length];
+
+while(number % 10 > 0)
+{
+    number_array[number % 10] = number / 10;
+    number = number % 10;
+}
+int sum = 0;
+
+foreach (int letter in number_array)
+{
+    sum += letter;
+}
