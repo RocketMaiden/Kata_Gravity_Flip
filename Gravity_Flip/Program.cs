02f92e62e650aa43a48657f92fe64a5591737863
RocketMaiden <elizabeth.ruchko@gmail.com>
2023-07-13 11:53:39 +0300

    removing task about numbers and letters

--- Gravity_Flip/Program.cs
+++ Gravity_Flip/Program.cs
@@ -8,33 +8,3 @@ boxes.ShowRawBoxes();
 
 Console.WriteLine();
 Console.ReadKey();
-
-int number = 1813;
-int length = 0;
-int[] number_array = new int[10];
-
-/*while(number%10 > 0)
-{
-    number = number % 10;
-    length++;
-}*/
-
-//number_array = new int[length];
-
-while(number % 10 > 0)
-{
-    number_array[number % 10] = number / 10;
-    number = number % 10;
-    length++;
-}
-int sum = 0;
-
-/*foreach (int letter in number_array)
-{
-    sum += letter;
-}*/
-
-for(int i = 0; i < length; i++)
-{
-    sum += number_array[i];
-}
