8d12366bf778bd7ee08e81cabfcdaa798977cd19
RocketMaiden <elizabeth.ruchko@gmail.com>
2023-08-03 21:54:29 +0300

    attempt to count boxes in horizontal rows

--- Gravity_Flip/Boxes.cs
+++ Gravity_Flip/Boxes.cs
@@ -2,41 +2,77 @@
 {
     public class Boxes
     {
-        private int[,] boxes_vertical;
+        private int[,] boxes;
         int width;
         int height;
+        
 
         public Boxes(int[] input_boxes_vertical)
         {
             width = input_boxes_vertical.Length;
             height = input_boxes_vertical.Max();
 
-            this.boxes_vertical = new int [width, height];
+            this.boxes = new int [width, height];
             
             for (int i = 0; i < width; i++)
             {
-                if (input_boxes_vertical[i] != 0)
+                //if (input_boxes_vertical[i] != 0)
                 {
                     for (int j = height - input_boxes_vertical[i]; j < height; j++)
                     {
-                        this.boxes_vertical[i, j] = 1;
+                        this.boxes[i, j] = 1;
                     }
                 }
             }
         }
 
-        public void ShowRawBoxes()
+        public int [] HorisontalBoxes()
+        {
+            int[] boxes_horisontal = new int[width];
+
+            for (int i = 0; i < width; i++)
+            {
+                int count = 0;
+
+                for (int j = 0; j < height; j++)
+                {
+                    if (boxes[i, j] == 1)
+                    {
+                        count++;
+                    }
+                    boxes_horisontal[j] = count;//
+                }
+            }
+            return boxes_horisontal;
+        }
+
+        public void MoveRight()
+        {
+
+        }
+
+        public void MoveLeft()
+        {
+
+        }
+
+        public void ShowBoxes()
         {
             for (int j = 0; j < height; j++)
             {
                 for (int i = 0; i < width; i++)
                 {
-                    Console.Write(boxes_vertical[i, j]);
+                    Console.Write(boxes[i, j]);
                 }
                 Console.WriteLine();
             }
-
-            
         }
+
+        public int[,] GetBoxes()
+        {
+            return boxes;
+        }
+
+
     }
 }
--- Gravity_Flip/Program.cs
+++ Gravity_Flip/Program.cs
@@ -4,7 +4,18 @@ Console.WriteLine("This is a box moving simulation");
 
 Boxes boxes = new Boxes(new int[] { 3, 2, 1, 2 });
 
-boxes.ShowRawBoxes();
+boxes.ShowBoxes();
+
+boxes.HorisontalBoxes();
+int[] horisontal_boxes_beforeGravityMove = boxes.HorisontalBoxes();
+Console.WriteLine("Here are counted boxes in each horisontal row:");
+foreach (int item in horisontal_boxes_beforeGravityMove)
+{
+    Console.Write($"{item}, ");
+}
+Console.WriteLine();
+
+boxes.ShowBoxes();
 
 Console.WriteLine();
 Console.ReadKey();
